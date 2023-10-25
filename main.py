c4e9f9d6ff33dcb6c749f5b6bf1b290d9cfa537b
Saeel Gote <saeelg30@gmail.com>
2023-10-25 16:41:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import pickle
+import pdfplumber as pd
+import os
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
+#creating Sidebar
+with st.sidebar:
+    st.title("Chat with PDF 🗣️")
+    st.markdown('''
+    ## About
+    This is a LLM power bot build to answer your question from given PDF
+    \n Made by [saeel-g](https://github.com/saeel-g)
+    \n Find the code on [GitHub](https://github.com/saeel-g/ChatPDF-Bot)
+''')
+    
+api_key = auth_key
+
+def main():
+    st.header('Chat with PDF 🗣️')
+    pdf_file = st.file_uploader("Upload your PDF file", type='pdf')
+   
+   # Reading the pdf and extracting the text
+    if pdf_file is not None:
+        read_pdf= pd.open(pdf_file)
+        text=''
+        for page in read_pdf.pages:
+            text += page.extract_text()
+
+        #Splitting the text into Chunks
+        text_splitter= RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunk=text_splitter.split_text(text=text)
+        # st.write(chunk)
+       
+        # Embeddings
+        store_name=pdf_file.name[:-4]
+        
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f'{store_name}.pkl',"rb") as f:
+                VectorStore=pickle.load(f)
+            # st.write('Embeddings loaded')
+        
+        else:
+            embeddings=OpenAIEmbeddings()
+            VectorStore=FAISS.from_texts(chunk, embedding=embeddings)
+            with open(f"{store_name}.pkl", 'wb') as f:
+                pickle.dump(VectorStore, f)
+            # st.write("Embeddings Created")
+
+        #Accepting user's question
+       
+        query=st.text_input("please ask a question regarding given PDF:")
+        if query:
+            docs=VectorStore.similarity_search(query=query, k=3) 
+            llm=OpenAI(api_key=api_key,model_name='gpt-3.5-turbo') 
+            chain=load_qa_chain(llm=llm, chain_type='stuff')
+            response=chain.run(input_documents=docs, question=query)
+            st.write(response)
+
+
+
+if __name__=='__main__':
+    main()
+
---
+++ readme.md
@@ -0,0 +1,50 @@
+
+# ChatPDf Bot
+A chat bot made to answer questions from your PDFs.
+
+## Overview
+The Chat PDF App is a simple web application that allows users to upload PDF files and interact with a chatbot to ask questions and receive answers based on the content of the uploaded PDF.
+
+## Screenshot
+![Asking a sample question after uploading the PDF](Screenshot.png)
+
+## Features
+* **PDF Upload:** Users can upload their PDF files directly to the application.
+* **Chatbot Interface:** Users can ask questions in a chat interface, and the chatbot will provide answers based on the content of the uploaded PDF.
+* **Machine Learning Model:** The chatbot is powered by a machine learning model that processes and analyzes the PDF content.
+* **GitHub Repository:** The source code for this project is available on GitHub here.
+* **Screencast Tutorial:** You can watch a detailed explanation of the codebase in a screencast here.
+* **Cloud Deployment:** The application is deployed in a cloud environment, and you can access it here.
+
+## Getting Started
+
+To run this application locally, follow these steps:
+
+1. Clone the GitHub repository:
+```bash
+git clone https://github.com/saeel-g/ChatPDF.git
+```
+2. Install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
+3. create a ".env" file in the same project folder and inside it write:
+```bash
+OPENAI_API_KEY= Insert your key here
+```
+4. Insert following command in terminal
+```bash
+streamlit run main.py
+```
+5. Open your web browser and navigate to given local host url to use the app.
+
+# Usage
+1. Upload a PDF file using the "Upload PDF" button.
+2. Ask questions in the chat interface.
+3. The chatbot will analyze the PDF content and provide answers.
+
+# Support
+If you encounter any issues or have questions, please create a GitHub issue
+
+# Contact
+For any inquiries, you can contact us at saeelg30@gmail.com.
